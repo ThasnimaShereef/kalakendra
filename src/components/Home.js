a7db5009e2cb176ed4723c8ae2e0cc14ed655a01
Thasnima Shereef <thasnimashereef@gmail.com>
2024-09-11 03:04:08 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ import './Home.css'; // Add CSS for styling
 
 const Home = () => {
   return (
-    <div className="hero-page" style={{ backgroundImage: `url('/image/logo/kk.jpg')` }}>
+    <div className="hero-page" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/image/logo/kk.jpg)` }}>
       <div className="content-container">
-      <img src="/image/logo/lockup color green.png" alt="Kala Kendra Logo" className="home-logo" />
+      <img src={`${process.env.PUBLIC_URL}/image/logo/lockup color green.png`} alt="Kala Kendra Logo" className="home-logo" />
         <p>
           Kala Kendra is an innovative interior design firm creating stylish, functional spaces that 
           reflect clients' unique tastes. We transform every project into a personalized masterpiece.
